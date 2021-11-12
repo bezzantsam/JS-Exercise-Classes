652f7824c479c67798b47e733f36ee375bdda95d
bezzantsam <83626945+bezzantsam@users.noreply.github.com>
2021-11-11 19:15:52 -0800

    task 2

--- index.js
+++ index.js
@@ -73,8 +73,28 @@ class Person {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-class Car {}
-
+class Car {
+  constructor(model, mpg) {
+    this.model = model;
+    this.milesPerGallon = mpg;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank = this.tank + gallons;
+  }
+  drive(dist) {
+    const drivableMiles = this.tank * this.milesPerGallon;
+    if (dist <= drivableMiles) {
+      this.odometer = this.odometer + dist;
+      this.tank = this.tank - dist / this.milesPerGallon;
+    } else {
+      this.odometer = this.odometer + drivableMiles;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
+}
 /*
   TASK 3
     - Write a Lambdasian class.
